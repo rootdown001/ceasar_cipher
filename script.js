829b84d08576365eacfc1f8914baa30572bae764
Lance Anderson <lance.a.585@gmail.com>
2022-10-11 18:18:43 -0700

    Add
    - Create for loop for array from starting
    - Create for loop for each 4 letter section

--- script.js
+++ script.js
@@ -1,21 +1,37 @@
 function rot13(str) {
+    
+    // create array from str
+    let arr = str.split(" ");
+    console.log(arr);
 
-
+    // function to find alphabet charCode position after cipher
     function position(initChar) {
-      
-      if ((initChar + 13) > 26) {
-        return (initChar + 13) - 26;
-      } else {
-        return initChar + 13;
-      }
-    
+        // if statement to add 13 places, and return new position
+        if ((initChar + 13) > 26) {
+          return (initChar + 13) - 26;
+        } else {
+          return initChar + 13;
+        }
     }
+
+    // create loop to go through new array
+    for (let i = 0; i < arr.length; i++) {
+        console.log(arr[i]);
+
+        // create loop to go through 4 letter segment
+        for (let j = 0; j < 4; j++) {
+            
+        }
+
+    } 
+
+
     
     
     
     
 
-    
+
     
     
     console.log(position(26))
@@ -25,6 +41,6 @@ function rot13(str) {
     
     
       return str;
-    }
+}
     
-    rot13("SERR PBQR PNZC");
+rot13("SERR PBQR PNZC");

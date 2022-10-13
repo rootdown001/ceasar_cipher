f79345927cd34e07d644531e6549d1fbff64707c
Lance Anderson <lance.a.585@gmail.com>
2022-10-12 17:45:49 -0700

    Add
    Add code to finish nested for loop. Gives correct answer

--- script.js
+++ script.js
@@ -1,30 +1,81 @@
 function rot13(str) {
     
     // create array from str
-    let arr = str.split(" ");
-    console.log(arr);
+    let strArr = str.split(" ");
+    // declare array for 4 digit string segment
+    let segArr = [];
+    // declare array for final answer 
+    let endArr = [];
+    // declare array for final answer 
+    let finalStr = "";
+    console.log(strArr);
+
+
 
     // function to find alphabet charCode position after cipher
-    function position(initChar) {
+    function codeChange(num) {
+
+       
         // if statement to add 13 places, and return new position
-        if ((initChar + 13) > 26) {
-          return (initChar + 13) - 26;
+        if (num < 65 || num > 90) {
+            return num;
+        } else if ((num + 13) > 90) {
+            return (num + 13) - 26;
         } else {
-          return initChar + 13;
+            return num + 13;
         }
     }
 
-    // create loop to go through new array
-    for (let i = 0; i < arr.length; i++) {
-        console.log(arr[i]);
 
+
+    // create loop to go through new array
+    for (let i = 0; i < strArr.length; i++) {
+        
+        // assign string from 
+        let workingStr = strArr[i];
+        console.log(workingStr);
+        let endStr = "";
         // create loop to go through 4 letter segment
-        for (let j = 0; j < 4; j++) {
+        for (let j = 0; j < workingStr.length; j++) {
             
+            let tempLetter = "";
+            let newLetter = "";
+            let startCode = 0;
+            let endCode = 0;
+
+   
+            // choose letter from array string
+            tempLetter = workingStr[j];
+            console.log(tempLetter);
+
+            // unicode of letter
+            startCode = tempLetter.charCodeAt(0);
+            console.log(startCode);
+
+            // call codeChange to find new unicode
+            endCode = codeChange(startCode);
+            console.log(endCode);
+
+            // new letter from new unicode
+            newLetter = String.fromCharCode(endCode);
+            console.log(newLetter);
+   
+            endStr = endStr + newLetter;
+
+            console.log(endStr);
+   //         console.log(tempLetter)
+   //         console.log(endArr);
         }
+
+        // add this new string to new array
+        endArr.push(endStr);
+        console.log(endArr);
+
 
     } 
 
+    // join endArr to get final string
+    finalStr = endArr.join(" ");
 
     
     
@@ -34,13 +85,10 @@ function rot13(str) {
 
     
     
-    console.log(position(26))
+    //console.log(finalStr)
     
-    
-    
-    
-    
-      return str;
+    console.log(finalStr);
+    return finalStr;
 }
     
-rot13("SERR PBQR PNZC");
+rot13("SERR CVMMN!");
